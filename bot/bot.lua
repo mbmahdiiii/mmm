96002418809a3fe348b179344aac9371ff1a8583
mbmahdiiii <mbmahdi650@gmail.com>
2016-02-28 19:51:21 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -244,7 +244,7 @@ function create_config( )
       "italian_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {115887468},
     admin_users = {},
     disabled_channels = {}
   }
